352a65736101a6d71ad848fa9e30c998181b83db
YoonDahye08 <ydh_0801@naver.com>
2023-12-10 21:13:22 +0900

    1st

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,9 +1,15 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import EditTopicForm from '@/components/EditTopicForm'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import React from 'react'
 
 const getTopicById = async (id) => {
   const apiUrl = process.env.API_URL
+
   try {
     const res = await fetch(`${apiUrl}/api/topics/${id}`, {
+      method: 'GET',
       cache: 'no-store',
     })
     if (!res.ok) {
@@ -15,10 +21,20 @@ const getTopicById = async (id) => {
   }
 }
 
-export default async function EditTopic({ params }) {
+export default async function EditTopicPage({ params }) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/signIn')
+  }
+
   const { id } = params
   const { topic } = await getTopicById(id)
   const { title, description } = topic
 
-  return <EditTopicForm id={id} title={title} description={description} />
+  return (
+    <div>
+      <EditTopicForm id={id} title={title} description={description} />
+    </div>
+  )
 }
